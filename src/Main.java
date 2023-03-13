6bc6d80e331f925b5d76dae1fc4d743a1c8e8dd2
Tagir Murtazin <tr.murtazin@yandex.ru>
2023-03-13 14:38:46 +0300

    Создайте    метод    с    одним    целочисленным    параметром. Метод    должен определить, является ли последняя цифра числа семеркой и вернуть boolean значение.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+/*
+Создайте    метод    с    одним    целочисленным    параметром.
+Метод    должен определить, является ли последняя цифра числа семеркой и вернуть boolean значение.
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Введите целое число");
+        Scanner scanner = new Scanner(System.in);
+        int digit = scanner.nextInt();
+        scanner.close();
+        boolean result = isLastDigit(digit);
+        if (result) System.out.printf("Последняя цифра числа %d равна 7", digit);
+        else System.out.printf("Последняя цифра числа %d не равна 7", digit);
+    }
+
+    static boolean isLastDigit(int i) {
+        final int DIGIT = 7;
+        return (i % 10) == DIGIT;
     }
 }
